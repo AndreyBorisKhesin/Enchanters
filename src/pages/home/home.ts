1166e3d7410b1633cbe84cfa365f47d32cf4bd1c
AsicChen <a19c97@hotmail.com>
2017-09-23 14:12:20 -0400

    global variable as a service!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,36 @@
-import { FirebaseProvider } from './../../providers/firebase/firebase';
+import { FirebaseProvider } from '../../providers/firebase/firebase';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { WelcomePage } from '../welcome/welcome';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { UserProvider } from '../../providers/user/user';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  user: any;
 
   names: FirebaseListObservable<any[]>;
   newName = '';
 
-  constructor(public navCtrl: NavController, public firebaseProvider: FirebaseProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public firebaseProvider: FirebaseProvider,
+    public userProvider: UserProvider,
+  ) {
     this.names = this.firebaseProvider.getNames();
+  }
+
+  ionViewDidLoad() {
+    console.log(this.user);
   }
 
   addItem() {
     this.firebaseProvider.addItem(this.newName);
+    this.userProvider.setUser(this.newName);
   }
 
   removeItem(id) {
@@ -26,6 +38,9 @@ export class HomePage {
   }
 
   goToWelcomePage() {
+    // "authenticate and get name"
+    // this.userProvider.setUser();
+    // actually go to the welcome page
     this.navCtrl.push(WelcomePage);
   }
 
