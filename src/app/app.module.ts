80b98a7b19b8411dc7bd4a70c2524bf24ea5acdf
AsicChen <a19c97@hotmail.com>
2017-09-23 16:50:24 -0400

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { WelcomePage } from '../pages/welcome/welcome';
 import { SocialPage } from '../pages/social/social';
 import { BusinessPage } from '../pages/business/business';
@@ -33,10 +30,7 @@ const firebaseConfig = {
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage,
     WelcomePage,
     SocialPage,
     BusinessPage,
@@ -53,10 +47,7 @@ const firebaseConfig = {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage,
     WelcomePage,
     SocialPage,
     BusinessPage,
